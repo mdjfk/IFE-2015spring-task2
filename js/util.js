81f3b19503faa5abb9f4f9a7dccee2f673cfd44e
Name <email@example.com>
2019-09-05 17:43:48 +0800

    2019-09-05

--- js/util.js
+++ js/util.js
@@ -202,8 +202,35 @@ function isSiblingNode(element, siblingNode) {
 // 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
 function getPosition(element) {
     var position = {
-        x: element.scrollLeft,
-        y: element.scrollTop
+        x: element.offsetLeft,
+        y: element.offsetTop
     };
     return position;
 }
+
+// 实现一个简单的Query
+function $(selector) {
+    if (/^#/.test(selector)) {
+        var array;
+        if ((array = /^#(.+) \.(.+)/.exec(selector)) !== null ) {
+            document.getElementById(array[1]).getElementsByClassName(array[1])[0];
+        }
+    }
+}
+
+// 可以通过id获取DOM对象，通过#标示，例如
+$("#adom"); // 返回id为adom的DOM对象
+
+// 可以通过tagName获取DOM对象，例如
+$("a"); // 返回第一个<a>对象
+
+// 可以通过样式名称获取DOM对象，例如
+$(".classa"); // 返回第一个样式定义包含classa的对象
+
+// 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]"); // 返回第一个包含属性data-log的对象
+
+$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
+
+// 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
